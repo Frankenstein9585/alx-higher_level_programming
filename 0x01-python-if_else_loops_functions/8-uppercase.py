6b67411b5d49785e7a372790b5d5e5cd609f77fb
Donald Peters <Donny9585@gmail.com>
2023-01-03 17:57:44 +0000

    Done task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) in range(ord('a'), ord('z') + 1):
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end="")
+    print()
+
+
+uppercase('Best School 98 Battery Street')
