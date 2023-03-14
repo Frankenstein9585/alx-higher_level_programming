0715ce4ead98791eea31d830833fff7864d3b58d
Frankenstein9585 <donny9585@gmail.com>
2023-03-14 19:15:27 +0100

    Done task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- creates a new table and adds multiple rows to it
+CREATE TABLE IF NOT EXISTS second_table (
+id INT,
+name VARCHAR(256),
+score INT
+);
+INSERT INTO second_table VALUES 
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
