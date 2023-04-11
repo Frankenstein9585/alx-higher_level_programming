04ac0fe3258380d88f445698b6d88b9e0625fcfb
Frankenstein9585 <donny9585@gmail.com>
2023-04-11 20:24:43 +0100

    Modified task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args[2]) {
+if (args.length === 2) {
+  console.log('No argument');
+} else if (args.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }
