3f0fbe7aeacecac1840719cb3cebde5e5f841a4c
Frankenstein9585 <donny9585@gmail.com>
2023-04-11 22:48:13 +0100

    Done task 6

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const args = process.argv;
+if (isNaN(Number(args[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < Number(args[2]); i++) {
+	  for (let j = 0; j < Number(args[2]); j++) {
+		  process.stdout.write('X');
+	  }
+	  process.stdout.write('\n');
+  }
+}
