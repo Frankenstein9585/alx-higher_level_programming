c4fc44c7418922e6de4ab6091d67e35488f066f1
Frankenstein9585 <donny9585@gmail.com>
2023-04-13 20:20:39 +0100

    Done task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-const max = Math.max(...args);
-const min = Math.min(...args);
-let second_max = 0;
+let max = args[0];
+let secondMax = args[1];
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
   for (let i = 0; i < args.length; i++) {
-    if (args[i] > min && args[i] < max) {
-      second_max = args[i];
+    if (args[i] > max) {
+      secondMax = max;
+      max = args[i];
+    } else if (args[i] > secondMax && args[i] < max) {
+      secondMax = args[i];
     }
   }
-  console.log(second_max);
+  console.log(secondMax);
 }
