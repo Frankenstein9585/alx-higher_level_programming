6dab4d30b75eaa5ea134e0b5379eff1c8b56ada4
Frankenstein9585 <donny9585@gmail.com>
2023-04-15 23:53:12 +0100

    Modified task 1 again

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """The main function; not to be executed if the script is imported"""
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * from states ORDER BY states.id ASC;")
     states = cursor.fetchall()
