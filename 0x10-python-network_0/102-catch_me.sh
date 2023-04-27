647a7300e01d1eccc4763249e8f48b0849396c2c
Frankenstein9585 <donny9585@gmail.com>
2023-04-27 23:02:18 +0100

    Modified task 12

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script makes a request to a URL and gets a response
-curl -s -H "X-Custom-Message: You got me!" 0.0.0.0:5000/catch_me 
-
+curl -s -H "X-Custom-Message: You got me!" 0.0.0.0:5000/catch_me
